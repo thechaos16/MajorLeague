1a0fbb3871a33338ca52b140b19b0334b6cb3990
Minkyu Kim <thechaos16@gmail.com>
2015-12-15 21:40:33 +0900

    random data splitter

--- WAR_Predictor/data_splitter.py
+++ WAR_Predictor/data_splitter.py
@@ -1,2 +1,23 @@
-import os,sys
-import data_gathering as dg
+import random
+
+class splitData:
+    ## option: {'method': 'random', 'ratio':0.7}
+    def __init__(self,data,opt):
+        self.data = data
+        self.opt = opt
+    
+    def splitRandom(self):
+        ## random sampling
+        upLimit = max(self.data)
+        downLimit = min(self.data)
+        testIdx = []
+        while True:
+            randNum = random.randint(downLimit,upLimit)
+            if randNum not in testIdx:
+                testIdx.append(randNum)
+            if len(testIdx)>=self.opt['ratio']*len(self.data):
+                return testIdx
+            
+    def splitByMethod(self):
+        if self.opt['method'].lower()=='random':
+            return self.splitRandom()
