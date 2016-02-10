fe01751b38b1acfa698fdd02ca105cdaba8737ac
unknown <>
2016-02-10 19:35:51 +0900

    sim_check_iteration

--- WAR_Predictor/matching_alg/sim_check_iter.py
+++ WAR_Predictor/matching_alg/sim_check_iter.py
@@ -5,10 +5,31 @@ Created on Wed Feb 10 19:13:50 2016
 @author: thech
 """
 
+import numpy as np
+import sys
+## for test
+try:
+    import WAR_Predictor.matching_alg.sim_check as si
+except ImportError:
+    sys.path.append('../../')
+    import WAR_Predictor.matching_alg.sim_check as si
+ 
+
 class SimCheckIteration:
     def __init__(self,vec1,vec2,opt):
         self.vec1 = vec1
         self.vec2 = vec2
         self.opt = opt
     
-    
+    def run(self):
+        result_list = []
+        for i in range(len(self.vec1)):
+            similarity_check = si.SimCheck(self.vec1[i:],self.vec2[i:],self.opt)
+            error = similarity_check.run_by_option()
+            result_list.append(error)
+        ## should be improved
+        return np.mean(result_list)
+            
+if __name__=='__main__':
+    kk = SimCheckIteration([[1],[2],[3]],[[1],[2],[5]],'kl')
+    print(kk.run())
