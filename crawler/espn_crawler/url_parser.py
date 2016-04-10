839682dde071a5f42fda4cef9d781375459b56e2
unknown <>
2016-04-10 23:08:03 +0900

    little update for crawler

--- crawler/espn_crawler/url_parser.py
+++ crawler/espn_crawler/url_parser.py
@@ -14,22 +14,31 @@ class URLHandler(HTMLParser):
         super().__init__()
         self.url = url
         self.urldata = urlopen(url)
+        self.illegal_string_list = ['<','>','{','}','=','|',',']
     
     ## make url as string
     def url_parser(self):
+        # it should be upgraded by auto-checking encoding
         new_url_data = str(self.urldata.read().decode('cp1251'))
         return new_url_data
         
     ## parse only data
     def handle_data(self, data):
-        if 'Perez' in data:
-            print(data)
+        check = True
+        for string in self.illegal_string_list:
+            if string in data:
+                check = False
+                break
+        if check:
+            data = data.replace('\n','')
+            data = data.replace('\t','')
+            if len(set(data))>1:
+                print(data)
        
 if __name__=='__main__':
     mm = URLHandler('http://espn.go.com/mlb/playbyplay?gameId=350615102')
     #mm = URLHandler('https://docs.python.org/3/library/html.parser.html')
     
     dd = mm.url_parser()
-    mm.feed(dd)
-    
+    mm.feed(dd)    
     
